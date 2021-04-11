7ced3ef80807ffac3ac7e227dd622e24b1af6664
Carlos Berti <carlosberti182@gmail.com>
2021-04-11 02:04:46 -0300

    feat: add type to pages

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,7 +1,8 @@
 import client from 'graphql/client'
+import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql'
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
 import { GetStaticProps } from 'next'
-import { useRouter } from 'next/dist/client/router'
+import { useRouter } from 'next/router'
 import PageTemplate, { PageTemplateProps } from 'templates/Pages'
 
 export default function Page({ heading, body }: PageTemplateProps) {
@@ -13,7 +14,7 @@ export default function Page({ heading, body }: PageTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -23,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
