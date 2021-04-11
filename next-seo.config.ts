4a0a0b8cb116b9d64401f7f5d796bdbd84ea4178
Carlos Berti <carlosberti182@gmail.com>
2021-04-11 05:25:13 -0300

    chore: create next-seo config file

---
+++ next-seo.config.ts
@@ -0,0 +1,12 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    site_name: 'My trips'
+  },
+  twitter: {
+    handle: '@_iicarlos',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
